1bb3c309383955748a9c02d2d74be94d806ecf2b
jk9358 <jk9358@srmist.edu.in>
2026-02-22 18:30:50 +0530

    feature (UC2): display OOPS banner using stars

--- OOPSBannerApp.java
+++ OOPSBannerApp.java
@@ -1,15 +1,12 @@
 public class OOPSBannerApp {
     public static void main(String[] args) {
-        String[] bannerLines = new String[7];
-        bannerLines[0] = String.join("", "  ***", " ***", " ****", " ****", " ");
-        bannerLines[1] = String.join("", " *", "   *", " *", "   *", " *", "   *", " *", "   *");
-        bannerLines[2] = String.join("", " *", "   *", " *", "   *", " *", "   *", " *", "   *");
-        bannerLines[3] = String.join("", " *", "   *", " *", "   *", " ****", " ****", " ");
-        bannerLines[4] = String.join("", " *", "   *", " *", "   *", " *", "    ", " ", "   ");
-        bannerLines[5] = String.join("", " *", "   *", " *", "   *", " *", "    ", " ", "   ");
-        bannerLines[6] = String.join("", "  ***", " ***", " *", "    ", " ****", " ");
-        for (String line : bannerLines) {
-            System.out.println(line);
-        }
+        // We use 7 lines to draw the letters O-O-P-S
+        System.out.println("  *** *** **** **** ");
+        System.out.println(" * * * * * * * ");
+        System.out.println(" * * * * * * * ");
+        System.out.println(" * * * * **** **** ");
+        System.out.println(" * * * * * *");
+        System.out.println(" * * * * * *");
+        System.out.println("  *** *** * **** ");
     }
 }
