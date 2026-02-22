0231584ca97c4f4006aaeacf55e2ce17f17cdef3
jk9358 <jk9358@srmist.edu.in>
2026-02-22 19:15:58 +0530

    feature (UC7): encapsulated patterns in a CharacterPattern class

--- OOPSBannerApp.java
+++ OOPSBannerApp.java
@@ -1,36 +1,35 @@
 
 public class OOPSBannerApp {
 
-    public static void main(String[] args) {
-        String[] bannerLines = {
-            String.join("", getO(0), getO(0), getP(0), getS(0)),
-            String.join("", getO(1), getO(1), getP(1), getS(1)),
-            String.join("", getO(2), getO(2), getP(2), getS(2)),
-            String.join("", getO(3), getO(3), getP(3), getS(3)),
-            String.join("", getO(4), getO(4), getP(4), getS(4)),
-            String.join("", getO(5), getO(5), getP(5), getS(5)),
-            String.join("", getO(6), getO(6), getP(6), getS(6))
-        };
+    static class CharacterPattern {
+        private char character;
+        private String[] pattern;
 
-        for (String line : bannerLines) {
-            System.out.println(line);
+        public CharacterPattern(char character, String[] pattern) {
+            this.character = character;
+            this.pattern = pattern;
+        }
+
+        public String getLine(int lineIndex) {
+            return pattern[lineIndex] + " ";
         }
     }
 
-   
+    public static void main(String[] args) {
+        CharacterPattern oPattern = new CharacterPattern('O', 
+            new String[]{"  ***", " * *", " * *", " * *", " * *", " * *", "  ***"});
+        CharacterPattern pPattern = new CharacterPattern('P', 
+            new String[]{" ****", " * *", " * *", " ****", " * ", " * ", " * "});
+        CharacterPattern sPattern = new CharacterPattern('S', 
+            new String[]{" ****", " * ", " * ", " ****", "     *", "     *", " **** "});
 
-    public static String getO(int line) {
-        String[] pattern = {"  ***", " * *", " * *", " * *", " * *", " * *", "  ***"};
-        return pattern[line] + " ";
-    }
-
-    public static String getP(int line) {
-        String[] pattern = {" ****", " * *", " * *", " ****", " * ", " * ", " * "};
-        return pattern[line] + " ";
-    }
-
-    public static String getS(int line) {
-        String[] pattern = {" ****", " * ", " * ", " ****", "     *", "     *", " **** "};
-        return pattern[line] + " ";
+        for (int i = 0; i < 7; i++) {
+            StringBuilder sb = new StringBuilder();
+            sb.append(oPattern.getLine(i));
+            sb.append(oPattern.getLine(i));
+            sb.append(pPattern.getLine(i));
+            sb.append(sPattern.getLine(i));
+            System.out.println(sb.toString());
+        }
     }
 }
