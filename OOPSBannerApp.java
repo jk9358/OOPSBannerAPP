4a914c2e3f89807ad92aaa2f00c830e878f697dc
jk9358 <jk9358@srmist.edu.in>
2026-02-23 14:19:31 +0530

    uc3 completed: refactored to use String.join

--- OOPSBannerApp.java
+++ OOPSBannerApp.java
@@ -1,7 +1,11 @@
-
-public class OOPSBannerApp {
-    public static void main(String[] args) {
-        // This line prints OOPS to the console
-        System.out.println("OOPS");
+public class BannerApp {
+    public static void main(String[] args) {        
+        System.out.println(String.join("", "  *** ", " *** ", " **** ", " **** "));
+        System.out.println(String.join("", " * *", " * *", " * *", " * "));
+        System.out.println(String.join("", " * *", " * *", " * *", " * "));
+        System.out.println(String.join("", " * *", " * *", " **** ", " *** "));
+        System.out.println(String.join("", " * *", " * *", " * ", "    * "));
+        System.out.println(String.join("", " * *", " * *", " * ", "    * "));
+        System.out.println(String.join("", "  *** ", " *** ", " * ", " **** "));
     }
 }
