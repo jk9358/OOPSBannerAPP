fd9529d8e9c2d0eb24cf7981e625afe019ead219
jk9358 <jk9358@srmist.edu.in>
2026-02-23 09:08:48 +0530

    Coded to Print OOPS to Console

--- OOPSBannerApp.java
+++ OOPSBannerApp.java
@@ -1,39 +1,7 @@
-import java.util.HashMap;
-import java.util.Map;
 
 public class OOPSBannerApp {
-
-    static class CharacterPattern {
-        private String[] pattern;
-
-        public CharacterPattern(String[] pattern) {
-            this.pattern = pattern;
-        }
-
-        public String getLine(int lineIndex) {
-            return pattern[lineIndex] + " ";
-        }
-    }
-
     public static void main(String[] args) {
-        Map<Character, CharacterPattern> patternMap = new HashMap<>();
-
-        patternMap.put('O', new CharacterPattern(new String[]{"  ***", " * *", " * *", " * *", " * *", " * *", "  ***"}));
-        patternMap.put('P', new CharacterPattern(new String[]{" ****", " * *", " * *", " ****", " * ", " * ", " * "}));
-        patternMap.put('S', new CharacterPattern(new String[]{" ****", " * ", " * ", " ****", "     *", "     *", " **** "}));
-
-        String word = "OOPS";
-
-        for (int i = 0; i < 7; i++) {
-            StringBuilder sb = new StringBuilder();
-            
-            for (char c : word.toCharArray()) {
-                CharacterPattern cp = patternMap.get(c);
-                if (cp != null) {
-                    sb.append(cp.getLine(i));
-                }
-            }
-            System.out.println(sb.toString());
-        }
+        // This line prints OOPS to the console
+        System.out.println("OOPS");
     }
 }
