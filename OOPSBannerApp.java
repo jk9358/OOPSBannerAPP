e8beec33709cc2ce4ed5ab559c60242650e4b403
jk9358 <jk9358@srmist.edu.in>
2026-03-10 21:37:11 +0530

    uc6 completed: refactored to use static helper methods for character patterns

--- OOPSBannerApp.java
+++ OOPSBannerApp.java
@@ -1,19 +1,21 @@
-public class OOPSBannerApp {
-
+public class BannerApp {
     public static void main(String[] args) {
-        String[] bannerLines = new String[] {
-        String.join("", "    *    " , "    *    " , " ***    "   , "    *** "),
-        String.join("", "  *   *  " , "  *   *  " , " *     *  "   , "  **      "),
-        String.join("", " *     * " , " *     * " , " *      * "   , "**        "),
-        String.join("", " *     * " , " *     * " , " *     *  "   , " **       "),
-        String.join("", " *     * " , " *     * " , " ***    "   , "   *    "),
-        String.join("", " *     * " , " *     * " , " *         "   , "      *  "),
-        String.join("", " *     * " , " *     * " , " *         "   , "       * "),
-        String.join("", "  *   *  " , "  *   *  " , " *         "   , "     *   "),
-        String.join("", "    *    " , "    *    " , " *         "   , "**     ")
-        };
-        for (String line : bannerLines) {
-            System.out.println(line);
+        String[][] banner = { getO(), getO(), getP(), getS() };
+
+        for (int i = 0; i < 7; i++) {
+            System.out.println(String.join("", banner[0][i], banner[1][i], banner[2][i], banner[3][i]));
         }
+    }
+
+    public static String[] getO() {
+        return new String[] {" *** ", "* *", "* *", "* *", "* *", "* *", " *** "};
+    }
+
+    public static String[] getP() {
+        return new String[] {"**** ", "* *", "**** ", "* ", "* ", "* ", "* "};
+    }
+
+    public static String[] getS() {
+        return new String[] {" **** ", "* *", " **** ", "     *", "* *", " **** "};
     }
 }
